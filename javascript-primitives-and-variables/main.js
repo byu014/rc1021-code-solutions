c9bc72331726fc1e468a607be2ca1cde69e790f4
Bailey Yu <bailey.yu@yahoo.com>
2021-10-11 10:36:18 -0700

    finished javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable prefer-const */
+/* eslint-disable eol-last */
+let fullName = 'Bailey Yu';
+let isCool = true;
+let totalPets = 0;
+let unknown = null;
+let never;
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+console.log('value of unknown:', unknown);
+console.log('type of unknown:', typeof unknown);
+console.log('value of never:', never);
+console.log('typeof never:', never);

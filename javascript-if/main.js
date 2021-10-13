30cb57e0112cce8487e63b7a02b850b5ec5df99c
Bailey Yu <bailey.yu@yahoo.com>
2021-10-13 13:32:06 -0700

    finished javascript-if

---
+++ javascript-if/main.js
@@ -0,0 +1,65 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro
+ */
+
+function isUnderFive(num) {
+  if (num < 5) {
+    return true;
+  }
+  return false;
+}
+
+function isEven(num) {
+  return !(num % 2);
+}
+
+function startsWithJ(str) {
+  if (str[0] === 'J') {
+    return true;
+  }
+  return false;
+}
+
+function isOldEnoughToDrink(person) {
+  if (person.age >= 21) {
+    return true;
+  }
+  return false;
+}
+
+function isOldEnoughToDrive(person) {
+  if (person.age >= 16) {
+    return true;
+  }
+  return false;
+}
+
+function isOldEnoughToDrinkAndDrive(person) {
+  return false;
+}
+
+function categorizeAcidity(pH) {
+  if (pH >= 0 && pH < 7) {
+    return 'acid';
+  }
+  if (pH > 7 && pH <= 14) {
+    return 'base';
+  }
+  return 'neutral';
+}
+
+function introduceWarnerBro(name) {
+  if (name === 'yakko' || name === 'wakko') {
+    return 'We\'re the warner brothers!';
+  }
+  if (name === 'dot') {
+    return 'I\'m cute~';
+  }
+  return 'Goodnight everybody!';
+}

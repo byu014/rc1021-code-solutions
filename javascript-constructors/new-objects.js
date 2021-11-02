0495f2680d1d74dfad995301470b83a818f4a3c9
Bailey Yu <bailey.yu@yahoo.com>
2021-11-02 10:36:42 -0700

    finished javascript-constructors

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,19 @@
+let anObject = new Object();
+let anArray = new Array();
+let aNumber = new Number();
+let aString = new String();
+let aBoolean = new Boolean();
+let aFunction = new Function();
+
+console.log('value of anObject:', anObject);
+console.log('type of anObject:', typeof anObject);
+console.log('value of anArray:', anArray);
+console.log('type of anArray:', typeof anArray);
+console.log('value of aNumber:', aNumber);
+console.log('type of aNumber:', typeof aNumber);
+console.log('value of aString:', aString);
+console.log('type of aString:', typeof aString);
+console.log('value of aBoolean:', aBoolean);
+console.log('type of aBoolean:', typeof aBoolean);
+console.log('value of aFunction:', aFunction);
+console.log('type of aFunction:', typeof aFunction);

5bba7e0313c91949a9d0086673b4c5f0bb57502c
Bailey Yu <bailey.yu@yahoo.com>
2021-11-05 12:54:06 -0700

    finished javascript-ajax

--- javascript-ajax/users.js
+++ javascript-ajax/users.js
@@ -5,8 +5,6 @@ xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
 xhr.send();
 
 xhr.addEventListener('load', function () {
-  console.log(xhr.status);
-  console.log(xhr.response);
   for (let response of xhr.response) {
     const $li = document.createElement('li');
     $li.textContent = response.name;

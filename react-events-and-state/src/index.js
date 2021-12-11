bb9332044a4f35f4c38b37d9d79ac1129c68b98b
Bailey Yu <bailey.yu@yahoo.com>
2021-12-10 18:20:04 -0800

    finished react-events-and-state

---
+++ react-events-and-state/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CustomButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isClicked: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  render() {
+    if (this.state.isClicked) {
+      return <button onClick={this.handleClick}>Thanks!</button>;
+    }
+    return <button onClick={this.handleClick}>Click Me!</button>;
+
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.setState({ isClicked: true });
+  }
+
+}
+
+ReactDOM.render(<CustomButton/>, document.querySelector('#root'));

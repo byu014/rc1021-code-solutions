d6f382c87da365c4b878c558d6b19450530a4750
Bailey Yu <bailey.yu@yahoo.com>
2021-12-13 15:43:45 -0800

    finished react-toggle-switch-component

---
+++ react-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+npm init -y
+npm i react react-dom
+npm i --save-dev webpack webpack-cli babel-loader @babel/core @babel/plugin-transform-react-jsx
+mkdir dist src
+touch dist/index.html src/index.jsx

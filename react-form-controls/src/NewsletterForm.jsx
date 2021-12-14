b1b05b8d24b74c653e87b3a48767f36e0d10dfa5
Bailey Yu <bailey.yu@yahoo.com>
2021-12-14 10:55:35 -0800

    finished react-form-controls

---
+++ react-form-controls/src/NewsletterForm.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class NewsletterForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { input: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ input: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    console.log(this.state.input);
+    return (
+      <form onSubmit={this.handleSubmit} action="">
+        <label htmlFor="email">Email</label>
+        <input onChange={this.handleChange} type="text" name="email" id="email" />
+        <button type="submit">Sign Up</button>
+      </form>
+    );
+  }
+}
+
+export default NewsletterForm;
---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NewsletterForm from './NewsletterForm';
+
+ReactDOM.render(
+  <NewsletterForm />,
+  document.querySelector('#root')
+);

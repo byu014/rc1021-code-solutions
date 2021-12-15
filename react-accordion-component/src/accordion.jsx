f44582878ef1f75eb03d67aebedef647a60db947
Bailey Yu <bailey.yu@yahoo.com>
2021-12-15 12:57:35 -0800

    updated code to remove usage of query selectors

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -4,14 +4,7 @@ class Accordion extends React.Component {
   constructor(props) {
     super(props);
     this.state = { current: null };
-    this.topics = this.props.topics.map(topic => {
-      return (
-        <li className='topic' key={topic.id}>
-          <button id={topic.id} className='topic-button'>{topic.name}</button>
-          <p className='detail inactive'>{topic.detail}</p>
-        </li>
-      );
-    });
+    this.topics = [];
     this.handleClick = this.handleClick.bind(this);
   }
 
@@ -25,19 +18,15 @@ class Accordion extends React.Component {
     }
   }
 
-  setInactive() {
-    const $topics = document.querySelectorAll('.topic');
-    for (let $topic of $topics) {
-      if (this.state.current === $topic.querySelector('.topic-button').id) {
-        $topic.querySelector('.detail').classList.remove('inactive');
-      } else {
-        $topic.querySelector('.detail').classList.add('inactive');
-      }
-    }
-  }
-
   render() {
-    this.setInactive();
+    this.topics = this.props.topics.map(topic => {
+      return (
+        <li className='topic' key={topic.id}>
+          <button id={topic.id} className='topic-button'>{topic.name}</button>
+          <p className={`detail ${Number.parseInt(this.state.current) === topic.id ? '' : 'inactive'}`}>{topic.detail}</p>
+        </li>
+      );
+    });
     return (
       <div onClick={this.handleClick} className="accordion">
         <ul>

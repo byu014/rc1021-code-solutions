fdfb525aa5600c0688d341cf6fea0b56bdefcee6
Bailey Yu <bailey.yu@yahoo.com>
2021-12-14 22:41:53 -0800

    updated reactsetup script

--- react-setup.sh
+++ react-setup.sh
@@ -5,18 +5,17 @@ npm i --save-dev webpack webpack-cli babel-loader @babel/core @babel/plugin-tran
 mkdir dist src
 touch dist/index.html dist/styles.css src/index.jsx
 cp ../webpack.config.js ./webpack.config.js
-echo "<!DOCTYPE html> \
-<html lang="en"> \
-<head> \
-  <meta charset="UTF-8"> \
-  <meta http-equiv="X-UA-Compatible" content="IE=edge"> \
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"> \
-  <title>Document</title> \
-</head> \
-<body> \
-  <div id="root"></div> \
-  <script src="main.js"></script> \
-</body> \
-</html> \
-" > src/index.html \
-/
+echo -e '<!DOCTYPE html>\n' \
+'<html lang="en">\n' \
+'<head>\n'\
+'  <meta charset="UTF-8">\n '\
+'  <meta http-equiv="X-UA-Compatible" content="IE=edge">\n' \
+'  <meta name="viewport" content="width=device-width, initial-scale=1.0">\n' \
+'  <link rel="stylesheet" href="styles.css">\n' \
+'  <title>Document</title>\n '\
+'</head>\n' \
+'<body>\n' \
+'  <div id="root"></div>\n' \
+'  <script src="main.js"></script>\n '\
+'</body>\n' \
+'</html>' > dist/index.html \
